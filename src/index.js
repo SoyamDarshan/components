98d5e43467d393b0020eb22e3a600692dd491474
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-23 17:06:50 +0530

    using fakerJS to send parameterized values to CommentDetail

--- src/index.js
+++ src/index.js
@@ -6,12 +6,39 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail 
+            author={ faker.name.firstName() }
+            timeAgo="Today at 4:45PM" 
+            image_src={ faker.image.avatar() } 
+            comment={ faker.hacker.phrase() }
+            />
+            <CommentDetail 
+            author={ faker.name.firstName() }
+            timeAgo="Yesterday at 3:45AM" 
+            image_src={ faker.image.avatar() } 
+            comment={ faker.hacker.phrase() }
+            />
+            <CommentDetail author={ faker.name.firstName() }
+            timeAgo="Today at 4:45AM" 
+            image_src={ faker.image.avatar() } 
+            comment={ faker.hacker.phrase() }
+            />
+            <CommentDetail author={ faker.name.firstName() } 
+            timeAgo="Today at 1:45PM" 
+            image_src={ faker.image.avatar() } 
+            comment={ faker.hacker.phrase() }
+            />
+            <CommentDetail author={ faker.name.firstName() }
+            timeAgo="Yesterday at 2:45PM" 
+            image_src={ faker.image.avatar() } 
+            comment={ faker.hacker.phrase() }
+            />
+            <CommentDetail 
+            author={ faker.name.firstName() } 
+            timeAgo="Today at 5:45PM"
+            image_src={ faker.image.avatar() }  
+            comment={ faker.hacker.phrase() }
+            />
         </div>
     );
 };
