929a27ddced2e7303f98e58d01d53634cbea9fed
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-23 20:37:08 +0530

    added ApprovalCard.js

--- README.md
+++ README.md
@@ -66,3 +66,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+### FakerJS
+To access fakerJS for random data use `npm install --save faker`
+and then you will be able to access faker by importing it eg:- `import faker from 'faker';`
--- src/index.js
+++ src/index.js
@@ -2,43 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-            author={ faker.name.firstName() }
-            timeAgo="Today at 4:45PM" 
-            image_src={ faker.image.avatar() } 
-            comment={ faker.hacker.phrase() }
-            />
-            <CommentDetail 
-            author={ faker.name.firstName() }
-            timeAgo="Yesterday at 3:45AM" 
-            image_src={ faker.image.avatar() } 
-            comment={ faker.hacker.phrase() }
-            />
-            <CommentDetail author={ faker.name.firstName() }
-            timeAgo="Today at 4:45AM" 
-            image_src={ faker.image.avatar() } 
-            comment={ faker.hacker.phrase() }
-            />
-            <CommentDetail author={ faker.name.firstName() } 
-            timeAgo="Today at 1:45PM" 
-            image_src={ faker.image.avatar() } 
-            comment={ faker.hacker.phrase() }
-            />
-            <CommentDetail author={ faker.name.firstName() }
-            timeAgo="Yesterday at 2:45PM" 
-            image_src={ faker.image.avatar() } 
-            comment={ faker.hacker.phrase() }
-            />
-            <CommentDetail 
-            author={ faker.name.firstName() } 
-            timeAgo="Today at 5:45PM"
-            image_src={ faker.image.avatar() }  
-            comment={ faker.hacker.phrase() }
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+                </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author={ faker.name.firstName() }
+                timeAgo="Today at 4:45PM" 
+                image_src={ faker.image.avatar() } 
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author={ faker.name.firstName() }
+                timeAgo="Yesterday at 3:45AM" 
+                image_src={ faker.image.avatar() } 
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() }
+                timeAgo="Today at 4:45AM" 
+                image_src={ faker.image.avatar() } 
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() } 
+                timeAgo="Today at 1:45PM" 
+                image_src={ faker.image.avatar() }
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author={ faker.name.firstName() }
+                timeAgo="Yesterday at 2:45PM" 
+                image_src={ faker.image.avatar() } 
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                author={ faker.name.firstName() } 
+                timeAgo="Today at 5:45PM"
+                image_src={ faker.image.avatar() }  
+                comment={ faker.hacker.phrase() }
+                />
+            </ApprovalCard>           
         </div>
     );
 };
